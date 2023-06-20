f5e58dbdf71fe350e312cbf89a3c163c2e07f21b
fairycreator <122693095+fairycreator@users.noreply.github.com>
2023-06-20 11:33:01 +0200

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const uniqueKey = '37376459-60d6298d1e87a90b85928e997'
-
+import { uniqueKey } from './pixaby_api.js'; 
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import { searchImages } from './pixaby_api.js';
 
 let searchQuery = '';
 let page = 1;
@@ -11,85 +11,84 @@ let loadMoreBtn = document.querySelector('.load-more');
 let backToTopBtn = document.querySelector('.back-to-top');
 let searchForm = document.querySelector('.search-form');
 
+loadMoreBtn.style.display = 'none';
+
 searchForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    searchQuery = event.target.searchInput.value;
-    page = 1;
-    gallery.innerHTML = '';
-    loadMoreBtn.style.display = 'none';
-    searchImages();
+  event.preventDefault();
+  searchQuery = event.target.searchInput.value;
+  page = 1;
+  gallery.innerHTML = '';
+  loadMoreBtn.style.display = 'none';
+  doSearch();
 });
 
-function searchImages() {
-let apiUrl = `https://pixabay.com/api/?key=${uniqueKey}&q=${encodeURIComponent(searchQuery)}&image_type=photo&orientation=horizontal&safesearch=true`;
+loadMoreBtn.addEventListener('click', function() {
+  page++;
+  doSearch();
+});
 
-fetch(apiUrl)
-    .then(response => response.json())
+function doSearch() {
+  searchImages(searchQuery, page)
     .then(data => {
-        let images = data.hits; // array of images from the response
+      let images = data.hits; // array of images from the response
 
-        if (images.length > 0) {
+      if (images.length > 0) {
         let imageCards = images.map(image => {
-            return `
-                <div class="card">
-                <a href="${image.largeImageURL}" data-lightbox="gallery">
+          return `
+            <div class="card">
+              <a href="${image.largeImageURL}" data-lightbox="gallery">
                 <img src="${image.webformatURL}" alt="${image.tags}">
-                </a>
-                <p>Likes: ${image.likes}</p>
-                <p>Views: ${image.views}</p>
-                <p>Comments: ${image.comments}</p>
-                <p>Downloads: ${image.downloads}</p>
-                </div>
-                `;
+              </a>
+              <p>Likes: ${image.likes}</p>
+              <p>Views: ${image.views}</p>
+              <p>Comments: ${image.comments}</p>
+              <p>Downloads: ${image.downloads}</p>
+            </div>
+          `;
         });
 
-    gallery.innerHTML += imageCards.join('');
+        gallery.insertAdjacentHTML('beforeend', imageCards.join(''));
 
         if (page === 1) {
-            let totalHits = data.totalHits; // total number of images
-            Notiflix.Notify.success = `Ура! Знайдено ${totalHits} зображень.`;
+          let totalHits = data.totalHits; // total number of images
+          Notiflix.Notify.success(`Ура! Знайдено ${totalHits} зображень.`);
         }
 
-        loadMoreBtn.style.display = 'block'; // Show the Load More button
-        page++; // Increment the page number for the next request
+        let lightbox = new SimpleLightbox('.gallery a'); // Initialize lightbox
+        
 
-        let lightbox = new SimpleLightbox('.gallery a'); // Initialize the simple lightbox
+        // Check pagination
+        let showBtn = page < Math.ceil(data.totalHits / 12);
+        if (showBtn) {
+          loadMoreBtn.style.display = 'block'; // Show LM
         } else {
+          loadMoreBtn.style.display = 'none'; // Hide LM
         let message = document.createElement('div');
-        message.classList.add('message');
-        message.textContent = 'На жаль, зображень не знайдено.';
-        gallery.appendChild(message);
+          message.classList.add('message');
+          message.textContent = "We're sorry, but you've reached the end of search results.";
+          gallery.appendChild(message);
         }
-
-              // Check if there are more images available for pagination
-        if (page <= data.total / 40) {
-            loadMoreBtn.style.display = 'block';
-        } else {
-            loadMoreBtn.style.display = 'none';
-            let message = document.createElement('div');
-            message.classList.add('message');
-            message.textContent = "We're sorry, but you've reached the end of search results.";
-            gallery.appendChild(message);
-        }
+      } else {
+        let message = document.createElement('div');
+          message.classList.add('message');
+          message.textContent = "На жаль, зображень не знайдено.";
+          loadMoreBtn.style.display = 'none'; // Hide LM
+      }
     })
     .catch(error => console.log(error));
 }
 
-loadMoreBtn.addEventListener('click', function() {
-    searchImages();
-});
-
 backToTopBtn.addEventListener('click', function() {
-    window.scrollTo({
+  window.scrollTo({
     top: 0,
     behavior: 'smooth'
-    });
+  });
 });
-
+  
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 500) {
-        backToTopBtn.style.display = 'block';
-    } else {
-        backToTopBtn.style.display = 'none';
-    }
+  if (window.scrollY > 500) {
+    backToTopBtn.style.display = 'block';
+  } else {
+    backToTopBtn.style.display = 'none';
+  }
 });
--- src/pixaby_api.js
+++ src/pixaby_api.js
@@ -1,1 +1,9 @@
-const uniqueKey = '37376459-60d6298d1e87a90b85928e997'
+export const uniqueKey = '37376459-60d6298d1e87a90b85928e997';
+
+export function searchImages(searchQuery, page) {
+    const apiUrl = `https://pixabay.com/api/?key=${uniqueKey}&q=${encodeURIComponent(searchQuery)}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    return fetch(apiUrl)
+        .then(response => response.json())
+        .catch(error => console.log(error));
+    }
